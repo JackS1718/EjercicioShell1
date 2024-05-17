8ba0f2393cb53e077a22beecf85d47c42be1cb8b
jacks1718 <javak.maravi@gmail.com>
2024-05-17 11:43:23 +0200

    new1

---
+++ VARIABLE/Ejercicio2.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+read -p "Introduce tu actividad favorita" Actividad
+read -p "Introduce tu comida favorita" Comida
+echo "Mi actividad favorita es $Actividad. Mi comida favorita es $Comida"
+
