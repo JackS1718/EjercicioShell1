1f969e9c8a6a6ce98cac8d65cd74fca7f1d4a18b
jacks1718 <javak.maravi@gmail.com>
2024-05-21 13:09:39 +0200

    actualizacion

--- CONDICIONALES/Ejercicio1.sh
+++ CONDICIONALES/Ejercicio1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "hoy es " ´date´
-echo -e "\ningresa al directorio"
-read the_path
-echo -e "\n tu ruta tiene los siguientes archivos y carpetas: "
-ls $the_path
+
+if [ "$(whoami)" != "root" ]; then
+
+echo "No tiene acceso como root"
+
+fi
---
+++ PARAMETROS/Ejercicio2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function parametros()
+{
+fichero="carpeta personal"
+echo "En el fichero $fichero existen" $# "parametros"
+
+}
+
+texto=$(parametros 1 2 3 4 5)
+echo $texto
